1abf51a98588b7015ee0c220bb5e49787aa5bc49
BuildTools <unconfigured@null.spigotmc.org>
2023-05-12 09:22:34 -0300

    FIX cadastro.php

--- cadastro.php
+++ cadastro.php
@@ -11,23 +11,23 @@
 </head>
 <body>
     <div class="container">
-        <h1 class="text-center mt-5 mb-5">Preencha os campos abaixo<h1>
+        <h1 class="text-center mt-5 mb-5">Preencha os campos abaixo</h1>
         <form action="cadastrar_funcionario.php" method="post">
             <div class="form-group">
                 <label for="nome">Nome:</label>
-                <input type="text" class="form-control" id="nome" placeholder="Digite seu nome">
+                <input type="text" class="form-control" name="nome" placeholder="Digite seu nome">
             </div>
             <div class="form-group">
                 <label for="email">Email:</label>
-                <input type="email" class="form-control" id="email" placeholder="Digite seu email">
+                <input type="email" class="form-control" name="email" placeholder="Digite seu email">
             </div>
             <div class="form-group">
                 <label for="funcao">Função:</label>
-                <input type="password" class="form-control" id="funcao" placeholder="Digite sua função">
+                <input type="text" class="form-control" name="funcao" placeholder="Digite sua função">
             </div>
             <div class="form-group">
                 <label for="salario">Salário:</label>
-                <input type="password" class="form-control" id="salario" placeholder="Digite seu salário">
+                <input type="text" class="form-control" name="salario" placeholder="Digite seu salário">
             </div>
             <button type="submit" class="btn btn-primary">Cadastrar</button>
         </form>
