dc5c7796aca7b32658fd16494d82cda3c553c3ec
BuildTools <unconfigured@null.spigotmc.org>
2023-05-12 10:33:20 -0300

    FIX editar.php

--- editar.php
+++ editar.php
@@ -10,25 +10,35 @@
     <title>Cadastro</title>
 </head>
 <body>
+
+    <?php 
+        include("mysql_connection.php");
+        $sql_code = "SELECT * FROM funcionarios WHERE ID = '" . $_GET["id"] . "'";
+        $sql_query = $conn->query($sql_code) or die("Erro ao executar query: " . $conn->error);
+        $result = mysqli_query($conn, $sql_code);
+        $row = mysqli_fetch_assoc($result);
+    ?>
+
     <div class="container">
-        <h1 class="text-center mt-5 mb-5">Preencha os campos abaixo<h1>
+        <h1 class="text-center mt-5 mb-5">Preencha os campos abaixo</h1>
         <form action="update.php" method="post">
             <div class="form-group">
                 <label for="nome">Nome:</label>
-                <input type="text" class="form-control" id="nome" placeholder="Digite seu nome">
+                <input type="text" class="form-control" id="nome" name="nome" placeholder="Digite seu nome" value="<?php echo $row['nome']; ?>">
             </div>
             <div class="form-group">
                 <label for="email">Email:</label>
-                <input type="email" class="form-control" id="email" placeholder="Digite seu email">
+                <input type="email" class="form-control" id="email" name="email" placeholder="Digite seu email" value="<?php echo $row['email']; ?>">
             </div>
             <div class="form-group">
                 <label for="funcao">Função:</label>
-                <input type="password" class="form-control" id="funcao" placeholder="Digite sua função">
+                <input type="text" class="form-control" id="funcao" name="funcao" placeholder="Digite sua função" value="<?php echo $row['funcao']; ?>">
             </div>
             <div class="form-group">
                 <label for="salario">Salário:</label>
-                <input type="password" class="form-control" id="salario" placeholder="Digite seu salário">
+                <input type="text" class="form-control" id="salario" name="salario" placeholder="Digite seu salário" value="<?php echo $row['salario']; ?>">
             </div>
+            <input type="hidden" name="id" id="id" value="<?php echo $row['id']; ?>">
             <button type="submit" class="btn btn-primary">Cadastrar</button>
         </form>
     </div>
